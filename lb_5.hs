ebed1f1c4114ea82ce83d79c083d04776ea4f276
Belbli <andey.dyokin@gmail.com>
2020-02-26 01:37:25 +0300

    5.2 almost done

--- lb_5.hs
+++ lb_5.hs
@@ -1,3 +1,4 @@
+import Data.List
 data Product = Book String String | VideoCassete String | CD String String Integer deriving (Show, Eq)
 {-
     let c = Book "qwe" "asd"
@@ -41,6 +42,45 @@ data Value = Two | Three | Four | Five | Six | Seven
 type Card = (Value, Suit)
 type Deck = [Card]
 
+{-
+    let c1 = (Two, Club)
+    let c2 = (Ace, Club)
+    let c3 = (Two, Spade)
+    let list = c1 : c2 : c3 : []
+-}
+
 isMinor :: Card -> Bool
 isMinor (v, _) = if(v /= Jack && v /= Queen && v /= King && v /= Ace) then True else False
 
+sameSuit :: Card -> Card -> Bool
+sameSuit (_, s1) (_, s2) = s1 == s2
+
+beats :: Card -> Card -> Bool
+beats (v1, s1) (v2, s2) = if(sameSuit (v1, s1) (v1, s2) && v1 > v2) then True else False
+
+beats2 :: Suit -> Card -> Card -> Bool
+beats2 trump (v1, s1) (v2, s2) = if(sameSuit (v1, s1) (v1, s2) && v1 > v2) then True else if (s1 == trump) then True else False
+
+beatsList :: [Card] -> Card -> Suit -> [Card]
+beatsList [] _ _ = []
+beatsList (x:xs) card trump = if(beats2 trump card x) then x : beatsList xs card trump else beatsList xs card trump
+
+cardValue :: Card -> Integer
+cardValue (v, _) = case v of
+    Two -> 2
+    Three -> 3
+    Four -> 4
+    Five -> 5
+    Six -> 6
+    Seven -> 7
+    Eight -> 8
+    Nine -> 9
+    Ten -> 10
+    Jack -> 10
+    Queen -> 10
+    King -> 10
+    Ace -> 11
+
+ochko :: [Card] -> [Integer]
+ochko [] = []
+ochko (x:xs) = cardValue x : ochko xs
